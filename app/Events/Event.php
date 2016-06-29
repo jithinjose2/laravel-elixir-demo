840d18e31967b919d389c85a7fcee0db5d2392c4
Jithin Jose <jithinjose2@gmail.com>
2016-06-29 17:26:54 +0000

    Laravel application added

---
+++ app/Events/Event.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace ElixirDemo\Events;
+
+abstract class Event
+{
+    //
+}
---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace ElixirDemo\Providers;
+
+use Illuminate\Contracts\Auth\Access\Gate as GateContract;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        'ElixirDemo\Model' => 'ElixirDemo\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any application authentication / authorization services.
+     *
+     * @param  \Illuminate\Contracts\Auth\Access\Gate  $gate
+     * @return void
+     */
+    public function boot(GateContract $gate)
+    {
+        $this->registerPolicies($gate);
+
+        //
+    }
+}
